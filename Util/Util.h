23851152939d5f363b53e28ddb8096e5a3eb1db5
faith <scwyxy@126.com>
2018-09-21 16:50:07 +0800

    Initial Util

---
+++ Util/Util.h
@@ -0,0 +1,13 @@
+//
+//  Util.h
+//  Util
+//
+//  Created by Faith on 2018/9/21.
+//  Copyright © 2018年 faith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Util : NSObject
+
+@end
---
+++ Util/Util.m
@@ -0,0 +1,13 @@
+//
+//  Util.m
+//  Util
+//
+//  Created by Faith on 2018/9/21.
+//  Copyright © 2018年 faith. All rights reserved.
+//
+
+#import "Util.h"
+
+@implementation Util
+
+@end
